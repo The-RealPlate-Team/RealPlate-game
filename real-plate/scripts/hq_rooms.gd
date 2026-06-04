acd057428c56fb1494af038a64b3ba2f48078b6e
Joshua Yang <166943317+Joshuewok-Minecraft-fun-Owner@users.noreply.github.com>
2026-06-03 17:41:55 -0700

    Update hq_rooms.gd with Debug Code
    Commented out all old code for future reference and added Debug Code provided by aand111111 on The Brainless Zone Discord Server. Still doesn't work. (btw aand111111 if you have a GitHub account pls tell me so I can mention you directly)

--- real-plate/scripts/hq_rooms.gd
+++ real-plate/scripts/hq_rooms.gd
@@ -1,14 +1,39 @@
+#extends Node3D
+#
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	if GameState.next_tp_point_name != "":
+#		var tp_point = get_node_or_null(GameState.next_tp_point_name)
+#		if tp_point:
+#			global_transform = tp_point.global_transform
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+#
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	if GameState.next_tp_point_name != "":
-		var tp_point = get_node_or_null(GameState.next_tp_point_name)
-		if tp_point:
-			global_transform = tp_point.global_transform
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+	if GameState.next_tp_point_name == "":
+		print("no tp point set")
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var tp_point = find_child(GameState.next_tp_point_name, true, false)
+	if tp_point == null:
+		print("tp point not found: ", GameState.next_tp_point_name)
+		return
+
+	var player = get_node_or_null("Player")
+	if player == null:
+		print("Player not found in hq_rooms")
+		return
+
+	var t = tp_point.global_transform
+	t.origin.y += 1.5
+	player.global_transform = t
+
+	print("moved PLAYER to ", tp_point.name, " pos: ", player.global_position)
